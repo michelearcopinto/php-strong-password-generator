d3bfa2bcfa5c97b35c33e289f26169e23f35b035
michelearcopinto <guestphantom1@gmail.com>
2024-02-27 17:08:45 +0100

    feat: password generation done

--- index.php
+++ index.php
@@ -1,37 +1,56 @@
 <?php
-$inserted_password = $_GET['inserted_password'];
-$yes_repetition = $_GET['yes_repetition'];
-$no_repetition = $_GET['no_repetition'];
+$length_password = $_GET['length_password'];
+$password_repetition = $_GET['password_repetition'];
 $letters_checked = $_GET['letters_checked'];
 $numbers_checked = $_GET['numbers_checked'];
 $symbols_checked = $_GET['symbols_checked'];
+$generated_password = '';
 
 
-var_dump($inserted_password, $yes_repetition, $no_repetition, $letters_checked, $numbers_checked, $symbols_checked);
+var_dump($inserted_password, $password_repetition, $letters_checked, $numbers_checked, $symbols_checked);
 
+while (strlen($generated_password) < $length_password) {
 
-$random_uppercase_letter = chr(rand(65, 90));
+    $random_uppercase_letter = chr(rand(65, 90));
 
-$random_lowercase_letter = chr(rand(97, 122));
+    $random_lowercase_letter = chr(rand(97, 122));
 
-$picked_number = rand(1, 4);
+    $random_number = rand(0, 9);
 
-if ($picked_number === 1) {
+    $picked_number = rand(1, 4);
 
-    $random_symbol = chr(rand(33, 47));
-} elseif ($picked_number === 2) {
+    if ($picked_number === 1) {
 
-    $random_symbol = chr(rand(58, 64));
-} elseif ($picked_number === 3) {
+        $random_symbol = chr(rand(33, 47));
+    } elseif ($picked_number === 2) {
 
-    $random_symbol = chr(rand(91, 96));
-} elseif ($picked_number === 4) {
+        $random_symbol = chr(rand(58, 64));
+    } elseif ($picked_number === 3) {
 
-    $random_symbol = chr(rand(123, 126));
+        $random_symbol = chr(rand(91, 96));
+    } elseif ($picked_number === 4) {
+
+        $random_symbol = chr(rand(123, 126));
+    }
+
+    if ($picked_number === 1 && $letters_checked === 'letters_checked') {
+
+        $generated_password .=  $random_uppercase_letter;
+    } elseif ($picked_number === 2 && $letters_checked === 'letters_checked') {
+
+        $generated_password .= $random_lowercase_letter;
+    } elseif ($picked_number === 3 && $numbers_checked === 'numbers_checked') {
+
+        $generated_password .= $random_number;
+    } elseif ($picked_number === 4 && $symbols_checked === 'symbols_checked') {
+
+        $generated_password .= $random_symbol;
+    }
 }
 
 
-var_dump($random_uppercase_letter, $random_lowercase_letter, $random_symbol)
+var_dump($random_uppercase_letter, $random_lowercase_letter, $random_symbol);
+var_dump($generated_password);
 ?>
 
 <!DOCTYPE html>
@@ -55,29 +74,32 @@ var_dump($random_uppercase_letter, $random_lowercase_letter, $random_symbol)
     <h1 class="text-capitalize text-secondary text-center mt-5">strong password generator</h1>
     <h2 class="text-capitalize text-center text-white">Genera una password sicura</h2>
     <div class="alert alert-primary container" role="alert">
-        A simple primary alert—check it out!
+        Password generata
     </div>
     <div class="alert alert-danger container" role="alert">
-        A simple danger alert—check it out!
+        Nessun parametro valido inserito
     </div>
 
     <div class="card container ps-5 py-5">
         <form action="index.php" method="GET" class="w-75 row d-flex">
             <div class="col-9">
-                <label for="inserted_password" class="form-label mb-4">Lunghezza password:</label>
+                <label for="length_password" class="form-label mb-4">Lunghezza password:</label>
+                <br>
+                <span>Password generata:</span>
                 <br>
                 <span>Consenti ripetizioni di uno o più caratteri:</span>
             </div>
             <div class="col-3">
-                <input type="password" class="form-control mb-3" id="inserted_password">
+                <input type="number" name="length_password" class="form-control mb-3" id="length_password">
+                <input type="text" class="form-control mb-3" <?php echo "value='$generated_password'" ?> readonly>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="yes_repetition" value="yes_repetition" id="yes_repetition" <?php if ($yes_repetition === 'yes_repetition') echo 'checked' ?>>
+                    <input class="form-check-input" type="radio" name="password_repetition" value="yes_repetition" id="yes_repetition" <?php if ($password_repetition === 'yes_repetition') echo 'checked' ?>>
                     <label class="form-check-label" for="yes_repetition">
                         Sì
                     </label>
                 </div>
                 <div class="form-check mb-4">
-                    <input class="form-check-input" type="radio" name="no_repetition" value="no_repetition" id="no_repetition" <?php if ($no_repetition === 'no_repetition') echo 'checked' ?>>
+                    <input class="form-check-input" type="radio" name="password_repetition" value="no_repetition" id="no_repetition" <?php if ($password_repetition === 'no_repetition') echo 'checked' ?>>
                     <label class="form-check-label" for="no_repetition">
                         No
                     </label>
