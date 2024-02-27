37e41ba94fca86e4cf281957fc671deb21725921
michelearcopinto <guestphantom1@gmail.com>
2024-02-27 16:39:37 +0100

    feat: characters logic done

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
+$inserted_password = $_GET['inserted_password'];
+$yes_repetition = $_GET['yes_repetition'];
+$no_repetition = $_GET['no_repetition'];
+$letters_checked = $_GET['letters_checked'];
+$numbers_checked = $_GET['numbers_checked'];
+$symbols_checked = $_GET['symbols_checked'];
 
+
+var_dump($inserted_password, $yes_repetition, $no_repetition, $letters_checked, $numbers_checked, $symbols_checked);
+
+
+$random_uppercase_letter = chr(rand(65, 90));
+
+$random_lowercase_letter = chr(rand(97, 122));
+
+$picked_number = rand(1, 4);
+
+if ($picked_number === 1) {
+
+    $random_symbol = chr(rand(33, 47));
+} elseif ($picked_number === 2) {
+
+    $random_symbol = chr(rand(58, 64));
+} elseif ($picked_number === 3) {
+
+    $random_symbol = chr(rand(91, 96));
+} elseif ($picked_number === 4) {
+
+    $random_symbol = chr(rand(123, 126));
+}
+
+
+var_dump($random_uppercase_letter, $random_lowercase_letter, $random_symbol)
 ?>
 
 <!DOCTYPE html>
@@ -14,11 +46,7 @@
 
     <style>
         body {
-            background-color: #001632;
-        }
-
-        .form-control {
-            width: fit-content;
+            /* background-color: #001632; */
         }
     </style>
 </head>
@@ -34,7 +62,7 @@
     </div>
 
     <div class="card container ps-5 py-5">
-        <form class="w-75 row d-flex">
+        <form action="index.php" method="GET" class="w-75 row d-flex">
             <div class="col-9">
                 <label for="inserted_password" class="form-label mb-4">Lunghezza password:</label>
                 <br>
@@ -43,27 +71,27 @@
             <div class="col-3">
                 <input type="password" class="form-control mb-3" id="inserted_password">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="yes-repetition" id="yes-repetition">
-                    <label class="form-check-label" for="yes-repetition">
+                    <input class="form-check-input" type="radio" name="yes_repetition" value="yes_repetition" id="yes_repetition" <?php if ($yes_repetition === 'yes_repetition') echo 'checked' ?>>
+                    <label class="form-check-label" for="yes_repetition">
                         Sì
                     </label>
                 </div>
                 <div class="form-check mb-4">
-                    <input class="form-check-input" type="radio" name="no-repetition" id="no-repetition">
-                    <label class="form-check-label" for="no-repetition">
+                    <input class="form-check-input" type="radio" name="no_repetition" value="no_repetition" id="no_repetition" <?php if ($no_repetition === 'no_repetition') echo 'checked' ?>>
+                    <label class="form-check-label" for="no_repetition">
                         No
                     </label>
                 </div>
                 <div class="mb-2 form-check">
-                    <input type="checkbox" name="letters_checked" class="form-check-input" id="letters_checked">
+                    <input type="checkbox" name="letters_checked" value="letters_checked" class="form-check-input" id="letters_checked" <?php if ($letters_checked === 'letters_checked') echo 'checked' ?>>
                     <label class="form-check-label" for="letters_checked">Lettere</label>
                 </div>
                 <div class="mb-2 form-check">
-                    <input type="checkbox" name="numbers_checked" class="form-check-input" id="numbers_checked">
+                    <input type="checkbox" name="numbers_checked" value="numbers_checked" class="form-check-input" id="numbers_checked" <?php if ($numbers_checked === 'numbers_checked') echo 'checked' ?>>
                     <label class="form-check-label" for="numbers_checked">Numeri</label>
                 </div>
                 <div class="mb-2 form-check">
-                    <input type="checkbox" name="symbols_checked" class="form-check-input" id="symbols_checked">
+                    <input type="checkbox" name="symbols_checked" value="symbols_checked" class="form-check-input" id="symbols_checked" <?php if ($symbols_checked === 'symbols_checked') echo 'checked' ?>>
                     <label class="form-check-label" for="symbols_checked">Simboli</label>
                 </div>
             </div>
